9b55af95596d5c33bcea9bd7983bbff714a10402
Melvin McMonagle <101975067+Melvin-McMonagle@users.noreply.github.com>
2023-09-08 14:40:50 +0200

    Update transform-methods.ts

--- src/transformations/transform-methods.ts
+++ src/transformations/transform-methods.ts
@@ -1,39 +1,53 @@
 import {
-  ArrowFunctionExpression, BlockStatement,
-  Collection, FunctionExpression,
-  Identifier,
-  JSCodeshift,
-  ObjectExpression,
-  ObjectMethod,
-  ObjectProperty
+    ArrowFunctionExpression,
+    BlockStatement,
+    Collection,
+    Identifier,
+    JSCodeshift,
+    ObjectExpression,
+    ObjectMethod,
+    ObjectProperty
 } from "jscodeshift";
 
 export function transformMethods(root: Collection<any>, j: JSCodeshift) {
-  let methods = root.find(j.ObjectProperty, {
-    key: {
-      name: 'methods'
-    }
-  });
-  methods.forEach(methodNodePath => {
-    const value = methodNodePath.node.value as ObjectExpression;
-
-    (value.properties as (ObjectMethod|ObjectProperty)[]).forEach(method => {
-      const identifier = j.identifier((method.key as Identifier).name);
-      const isObjectMethod = 'params' in method && 'body' in method && 'async' in method;
-      const { params, body, async, comments, returnType } = isObjectMethod ? method : (method as ObjectProperty).value as ArrowFunctionExpression;
-
-      const declaration = j.functionDeclaration(
-          identifier,
-          params,
-          body as BlockStatement
-      );
-      declaration.async = async;
-      declaration.comments = comments;
-      declaration.returnType = returnType;
-
-      root.insertAfter(declaration);
+    /* Findet die methods properties innerhalb des roots. */
+    let methods = root.find(j.ObjectProperty, {
+        key: {
+            name: 'methods'
+        }
     });
-  });
-
-  methods.remove();
+    /* Die Funktion nimmt eine Liste von "Method-Nodes" (methodNodePath) und iteriert durch sie. */
+    methods.forEach(methodNodePath => {
+        /* Der Wert der aktuellen Node (methodNodePath.node.value) wird als ObjectExpression interpretiert und in als value zwischengespeichert. */
+        const value: ObjectExpression = methodNodePath.node.value as ObjectExpression;
+        /* Iteration durch die properties von Value, die entweder vom Typ ObjectMethod oder ObjectProperty sind. */
+        (value.properties as (ObjectMethod | ObjectProperty)[]).forEach((method: ObjectProperty | ObjectMethod) => {
+            /* Der Name der Methode wird als Identifier für die neue Funktionsdeklaration gesetzt. */
+            const identifier: Identifier = j.identifier((method.key as Identifier).name);
+            /* Überprüfung, ob die method eine ObjectMethod ist, bzw ob die jeweiligen keys vorhanden sind.*/
+            const isObjectMethod = 'params' in method && 'body' in method && 'async' in method;
+            /* Setzt verschiedene values der method als Konstante, je nachdem ob sie ein ObjectMethod oder eine ArrowFunctionExpression ist. */
+            const {
+                params,
+                body,
+                async,
+                comments,
+                returnType
+            } = isObjectMethod ? method : (method as ObjectProperty).value as ArrowFunctionExpression;
+            /* Erstellt eine neue Funktionsdeklaration. */
+            const declaration = j.functionDeclaration(
+                identifier,
+                params,
+                body as BlockStatement
+            );
+            /* Fügt der Funktionsdeklaration zusätzliche Informationen wie async, comments und return types hinzu */
+            declaration.async = async;
+            declaration.comments = comments;
+            declaration.returnType = returnType;
+            /* Fügt die neue Funktiondeklaration dem root-AST hinzu.*/
+            root.insertAfter(declaration);
+        });
+    });
+    /* Entfernt das methods blockstatement vom root */
+    methods.remove();
 }
